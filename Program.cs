6fe25dc9ab0a471e9a8e99b30ba94e3cd97a2516
SergeiGorshenin <81926868+SergeiGorshenin@users.noreply.github.com>
2025-02-02 21:18:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using Hello;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Подключение к grpcbin (используется HTTPS)
+        using var channel = GrpcChannel.ForAddress("https://grpcb.in:443");
+        var client = new HelloService.HelloServiceClient(channel);
+
+        try
+        {
+            // Вызов метода SayHello
+            var request = new HelloRequest { Greeting = "gRPC" };
+            var response = await client.SayHelloAsync(request);
+
+            Console.WriteLine($"Ответ сервера: {response.Reply}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
